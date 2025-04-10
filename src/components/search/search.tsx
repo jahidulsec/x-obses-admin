208d186783cbee86a55220039d095302868c57d5
Jahidul Islam <jahidul.app@gmail.com>
2025-04-10 20:01:09 +0600

    feat: adding search filter

---
+++ src/components/search/search.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { SearchIcon } from "lucide-react";
+import { useRouter } from "next-nprogress-bar";
+import { usePathname, useSearchParams } from "next/navigation";
+import React, { FormEvent, useState } from "react";
+import { Button } from "../ui/button";
+
+function Search({
+  placeholder = "Search...",
+  type = "search",
+}: {
+  placeholder?: string;
+  type?: string;
+}) {
+  const searchParams = useSearchParams();
+  const [search, setSearch] = useState(searchParams.get("q") || "");
+
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const params = new URLSearchParams(searchParams);
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    if (search) {
+      params.set("search", search);
+      params.delete("page");
+    } else {
+      params.delete("search");
+      params.delete("page");
+    }
+    router.push(pathname + "?" + params.toString());
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="relative min-w-[8rem] flex-1 sm:flex-none"
+    >
+      <Input
+        type={type}
+        name="search"
+        id="search"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        className="pl-8 pr-12 w-full"
+        placeholder={placeholder}
+      />
+      <Label
+        htmlFor="search"
+        className="absolute top-[50%] -translate-y-[50%] left-3 text-gray-400"
+      >
+        <SearchIcon className="size-4" />
+      </Label>
+      <Button
+        type="submit"
+        size={"icon"}
+        className="absolute right-0 top-0 rounded-l-none"
+      >
+        <SearchIcon className="size-4" />
+      </Button>
+    </form>
+  );
+}
+
+export { Search as SearchForm };
--- src/components/sidebar/app-navbar.tsx
+++ src/components/sidebar/app-navbar.tsx
@@ -6,26 +6,32 @@ import { PanelLeft } from "lucide-react";
 import { Button } from "../ui/button";
 import { useSidebarContext } from "./sidebar";
 import { Breadcrump } from "../breadcrump/breadcrump";
+import { SearchForm } from "../search/search";
 
 const AppNavbar = () => {
   const { onToggle } = useSidebarContext();
 
   return (
-    <nav className="flex items-center gap-3 py-3 border-b mb-3 px-4 sticky top-0 bg-background sm:rounded-t-lg z-10">
-      <Button
-        variant={"ghost"}
-        size={"icon"}
-        onClick={onToggle}
-        className="p-0 w-4 h-4"
-      >
-        <PanelLeft />
-        <span className="sr-only">Toggle</span>
-      </Button>
+    <header className="w-full flex justify-between items-center gap-5 py-3 border-b mb-3 px-4 sticky top-0 bg-background sm:rounded-t-lg z-10">
+      <nav className="flex items-center gap-3 ">
+        <Button
+          variant={"ghost"}
+          size={"icon"}
+          onClick={onToggle}
+          className="p-0 w-4 h-4"
+        >
+          <PanelLeft />
+          <span className="sr-only">Toggle</span>
+        </Button>
 
-      <Separator orientation="vertical" className="h-5" />
+        <Separator orientation="vertical" className="h-5" />
 
-      <Breadcrump />
-    </nav>
+        <Breadcrump />
+      </nav>
+
+      {/* search section */}
+      <SearchForm />
+    </header>
   );
 };
 
